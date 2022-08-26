9d5c165e5f5ae10678360f5a77901b1159ea8d1b
ediicamcaz <edicamcaz2022@gmail.com>
2022-08-26 01:21:17 -0500

    menu

--- 4-JDKJava/PooJava/src/MiEjemploJava/Persona.java
+++ 4-JDKJava/PooJava/src/MiEjemploJava/Persona.java
@@ -23,3 +23,20 @@ public class Persona {
             System.out.println("Duerme");
         }
 }
+
+// package MiEjemploJava;
+//
+//public class EjemploPersona {
+//
+//    public static void main(String[] args) {
+//        Persona p = new Persona();
+//       p.setNombre("Dora");
+//       p.setEdad(32);
+//       p.setNss(14689);
+//        System.out.println("el valor de la variable de instancia p.nombre -> " + p.getNombre());
+//        System.out.println("el valor de la variable de instancia p.edad -> " + p.getEdad());
+//        System.out.println("el valor de la variable de instancia p.nss -> " + p.getNss());
+//
+//
+//    }
+//}
